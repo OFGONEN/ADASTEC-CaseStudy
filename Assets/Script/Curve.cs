5a8787f3fcf20b789bea969821d82359f1859130
ofgonen-home <ofgonen@gmail.com>
2023-04-14 04:31:17 +0300

    Curve: Initilize Cubic Beizer Path and Draw it

--- Assets/Script/Curve.cs
+++ Assets/Script/Curve.cs
@@ -4,12 +4,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using EasyButtons;
 
 public class Curve : MonoBehaviour
 {
 #region Fields
-    [ Min( 0.01f ) ]
-    public float curve_step = 0.01f;
+
+  [ Header( "Setup" ) ]
+    [ Min( 2 ) ] public int curve_control_point_count = 2;
+    [ Min( 0 ) ] public int curve_lane_offset_horizontal = 1;
+    [ Min( 0.005f ) ] public float curve_step = 0.01f;
+
+	[ SerializeField ] Vector3[] curve_control_point_array_left;
+	[ SerializeField ] Vector3[] curve_control_point_array_right;
 #endregion
 
 #region Properties
@@ -19,6 +26,19 @@ public class Curve : MonoBehaviour
 #endregion
 
 #region API
+	[ Button ]
+	public void CreateCurve()
+	{
+		var pointCount = 1 + ( curve_control_point_count - 1 ) * 3;
+		curve_control_point_array_left  = new Vector3[ pointCount ];
+		curve_control_point_array_right = new Vector3[ pointCount ];
+
+		for( var i = 0; i < pointCount; i++ )
+		{
+			curve_control_point_array_left [ i ] = transform.position + transform.forward * i;
+			curve_control_point_array_right[ i ] = transform.position + transform.forward * i + transform.right * curve_lane_offset_horizontal;
+		}
+	}
 #endregion
 
 #region Implementation
@@ -28,18 +48,32 @@ public class Curve : MonoBehaviour
 #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
-		var positionOne   = transform.GetChild( 0 ).position;
-		var positionTwo   = transform.GetChild( 1 ).position;
-		var positionThree = transform.GetChild( 2 ).position;
+		if( curve_control_point_array_left == null ) // Curve is not initiated
+			return;
 
-		Vector3 positionTemp = positionOne;
+		DrawCurve( curve_control_point_array_left );
+		DrawCurve( curve_control_point_array_right );
+	}
 
-		for( float i = curve_step; i <= 1; i += curve_step )
-        {
-			var curvedPoint = PointCalculations.ReturnCurvedPoint( positionOne, positionTwo, positionThree, i );
+	void DrawCurve( Vector3[] curveControlPoints )
+	{
+		for( var i = 0; i < curveControlPoints.Length - 3; i += 3 )
+		{
+			Vector3 positionTemp = curveControlPoints[ i ];
 
-			Handles.DrawLine( positionTemp, curvedPoint );
-			positionTemp = curvedPoint;
+			for( float delta = curve_step; delta <= 1; delta += curve_step )
+			{
+				var curvedPoint = PointCalculations.ReturnCubicBeizerCurvedPoint(
+					curveControlPoints[ i ],
+					curveControlPoints[ i + 1 ],
+					curveControlPoints[ i + 2 ],
+					curveControlPoints[ i + 3 ],
+					delta
+				);
+
+				Handles.DrawLine( positionTemp, curvedPoint );
+				positionTemp = curvedPoint;
+			}
 		}
 	}
 #endif
