9539e2c8495502a1c88eee8173903568b5f45aba
ofgonen-home <ofgonen@gmail.com>
2023-04-14 04:10:37 +0300

    PointCalculations: ReturnCubicBeizerCurvedPoint

--- Assets/Script/PointCalculations.cs
+++ Assets/Script/PointCalculations.cs
@@ -36,4 +36,24 @@ public class PointCalculations
 
 		return ReturnLerpedPoint( firstPoint, secondPoint, lerpValue );
 	}
+
+	public static Vector3 ReturnCubicBeizerCurvedPoint( Vector3 pointOne, Vector3 pointTwo, Vector3 pointThree, Vector3 pointFour, float lerpValue )
+	{
+#if UNITY_EDITOR
+		if( lerpValue < 0 || lerpValue > 1 )
+		{
+			Debug.LogError( "Lerp Value must be between 0 and 1" );
+			lerpValue = Mathf.Clamp( lerpValue, 0, 1 );
+		}
+#endif
+
+		var firstPoint  = ReturnLerpedPoint( pointOne, pointTwo, lerpValue );
+		var secondPoint = ReturnLerpedPoint( pointTwo, pointThree, lerpValue );
+		var thirdPoint  = ReturnLerpedPoint( pointThree, pointFour, lerpValue );
+
+		var firstMiddlePoint  = ReturnLerpedPoint( firstPoint, secondPoint, lerpValue );
+		var secondMiddlePoint = ReturnLerpedPoint( secondPoint, thirdPoint, lerpValue );
+
+		return ReturnLerpedPoint( firstMiddlePoint, secondMiddlePoint, lerpValue );
+	}
 }
